d0916acf7f27d17ba0013978247f8c3d8d930b8a
Andrew Tran <andrewtranmsw@gmail.com>
2022-02-28 09:29:57 -0800

    pushin' p

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,56 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// const x = 23;
+
+// const calcAge = (birthYear) => 2022 - birth;
+
+// console.log("hello yo");
+
+// const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+// const temperatures2 = [1, 2, 4, 8, -7, -9, 0, 22, 28, 30, 31, 32];
+
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const currentTemp = temps[i];
+//     if (typeof currentTemp !== "number") continue;
+
+//     if (currentTemp > max) max = currentTemp;
+//     if (currentTemp < min) min = currentTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+
+//Problem 2: Function should now receive two arrays of temperatures.
+
+//1) Understand the problem
+//  - With 2 arrays, should we implement functionality twice? Nope--just merge the two arrays.
+
+// 2) How do we merge two arrays?
+//From Google -- array.prototype.concat();
+
+// const joinedArr = temperatures.concat(temperatures2);
+// console.log(joinedArr);
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: Number(prompt("Degrees celsius:")),
+  };
+  console.log(measurement);
+  console.table(measurement);
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number!? ✨';
+
+// console.log(
+//   (document.querySelector('.message').textContent = 'Correct Number!? ✨')
+// );
+
+// //changes the value of the numbers
+// document.querySelector('.number').textContent = 14;
+// document.querySelector('.score').textContent = 69;
+// //changes the value of the input field
+// document.querySelector('.guess').value = 22;
+// console.log((document.querySelector('.guess').value = 22));
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'Not a valid number choice 😒';
+  }
+});
