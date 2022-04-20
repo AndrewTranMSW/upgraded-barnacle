adacc70b6a1531543eb0a26a163c2d4160c3f389
Andrew Tran <andrewtranmsw@gmail.com>
2022-04-20 16:00:50 -0700

    score logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -34,13 +34,39 @@ console.log('Ex 72');
 
 console.log('Ex 74');
 
-const number = Math.random();
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(typeof guess);
+  console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'Please enter a number 😒';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      "Ayo, that's the correct number 🎉";
+    score++;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high lol 🪁';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost da game bby';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low, sheesh 🍑';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'You hella lost da game bby';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
