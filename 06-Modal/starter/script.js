636611c054161710ad8d1a653676d9d60fdbb1ab
Andrew Tran <andrewtranmsw@gmail.com>
2022-04-21 16:36:18 -0700

    updates

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+console.log('Ex 80');
+
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+let btnCloseModal = document.querySelector('.close-modal');
+let btnOpenModal = document.querySelectorAll('.show-modal');
+
+//Function to close modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+//Function to open modal
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+//closes the modal on 'x' btn click
+btnCloseModal.addEventListener('click', closeModal);
+//closes the modal on overlay click
+overlay.addEventListener('click', closeModal);
+
+console.log('Ex 81');
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
