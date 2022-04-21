ce793509f63b026551e6c2c16b4d8d977e7be0af
Andrew Tran <andrewtranmsw@gmail.com>
2022-04-21 11:28:04 -0700

    Guess the number game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -32,41 +32,66 @@ console.log('Ex 72');
 // document.querySelector('.score').textContent = 69;
 // console.log((document.querySelector('.guess').value = 23));
 
-console.log('Ex 74');
+console.log('Ex 74 & 76 Coding Challenge');
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let hiScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'Please enter a number 😒';
+    //When there isn't a correct input
+    // document.querySelector('.message').textContent = 'Please enter a number 😒';
+    displayMessage('Please enter a number 😒');
+    document.querySelector('body').style.backgroundColor = 'purple';
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent =
-      "Ayo, that's the correct number 🎉";
+    //When the correct number was chosen
+    // document.querySelector('.message').textContent =
+    //   "Ayo, that's the correct number 🎉";
+    displayMessage("Ayo, that's the correct number 🎉");
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = 'green';
     score++;
     document.querySelector('.score').textContent = score;
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high lol 🪁';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost da game bby';
-      document.querySelector('.score').textContent = 0;
+    //Keeps track of the hiscore and checks it each time this function runs. Stores it in the .highscore class
+    if (score > hiScore) {
+      hiScore = score;
+      document.querySelector('.highscore').textContent = hiScore;
     }
-  } else if (guess < secretNumber) {
+  } else if (guess !== secretNumber) {
+    //Checks for the guess being too high or too low w/ ternary
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low, sheesh 🍑';
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? 'Too High 🪁' : 'Too Low 🍑';
+      displayMessage(guess > secretNumber ? 'Too High 🪁' : 'Too Low 🍑');
+      document.querySelector('body').style.backgroundColor = 'brown';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent =
-        'You hella lost da game bby';
+      displayMessage('You lost! 🤢');
+      document.querySelector('body').style.backgroundColor = 'pink';
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+//Select the element with the 'again' class and attach an event listener for a click.
+//Once clicked, restore initial values of the score and number variables.
+//Restore the initial conditions of the message, number, score and guess input field
+//Restore the original background color to #222 and number width of 15rem.
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.guess').value = '';
 });
