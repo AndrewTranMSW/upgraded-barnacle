6447163b3e3b0a59babf41ee01c2a92a50786403
Andrew Tran <andrewtranmsw@gmail.com>
2022-04-22 10:14:21 -0700

    pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-// Selecting elements 
+// Selecting elements
 let score0El = document.querySelector('#score--0');
 let score1El = document.getElementById('score--1');
 let current0El = document.getElementById('current--0');
@@ -19,19 +19,22 @@ let currentScore = 0;
 let activePlayer = 0;
 
 //Rolling dice functionality
-btnRoll.addEventListener('click', function() {
-    // 1. Generate a random dice roll
-    const dice = Math.trunc(Math.random() * 6 + 1);
-    console.log(dice);
-    // 2. Then display the dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
-    // 3. Check for a rolled '1'
-    if(dice !== 1){
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2. Then display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for a rolled '1'
+  if (dice !== 1) {
     currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore; 
-    } else {
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
     // 4. If '1' is true, switch to next player
-    } document.getElementById(`current--${activePlayer}`).textContent = 0; activePlayer = activePlayer === 0 ? 1 : 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
-})
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
