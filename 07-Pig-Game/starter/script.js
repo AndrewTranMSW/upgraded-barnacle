d5b52dadfe102ce76c79e78c9f1260d21a6de404
Phavy Chey <phavychey@Phavys-MacBook-Pro.local>
2022-04-21 23:09:23 -0700

    pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,37 @@
 'use strict';
+// Selecting elements 
+let score0El = document.querySelector('#score--0');
+let score1El = document.getElementById('score--1');
+let current0El = document.getElementById('current--0');
+let current1El = document.getElementById('current--1');
+let diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
-let playerOneCurrentScore = 0;
-let playerOneTotalScore = 0;
-let playerTwoCurrentScore = 0;
-let playerTwoTotalScore = 0;
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function() {
+    // 1. Generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6 + 1);
+    console.log(dice);
+    // 2. Then display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    // 3. Check for a rolled '1'
+    if(dice !== 1){
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore; 
+    } else {
+    // 4. If '1' is true, switch to next player
+    } document.getElementById(`current--${activePlayer}`).textContent = 0; activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+})
